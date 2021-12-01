60749e029c7bf5b2073d6ba5a4f102d908dca6c5
Avichai Levenson <avichailevenson@gmail.com>
2021-11-30 18:12:46 -0900

    Check if metamask is installed and render conditionally

--- xathon-react/src/App.tsx
+++ xathon-react/src/App.tsx
@@ -8,9 +8,15 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import { SiEthereum } from "react-icons/si";
 import React, { useState } from "react";
 import CreateWalkathonFactory from "./forms/CreateWalkathonFactory/CreateWalkathonFactory";
+import NoMetaMask from "./components/NoMetaMask/NoMetaMask";
 
+declare let window: any;
 const App: React.FC = () => {
   const [queryItems, setQueryItems] = useState<string[]>([]);
+  const [isMetaMaskActive] = useState<boolean>(
+    window.ethereum ? window.ethereum.isMetaMask : false
+  );
+
   return (
     <DarkModeProvider>
       <Page>
@@ -22,20 +28,24 @@ const App: React.FC = () => {
           <DarkModeToggle />
         </Navbar>
 
-        <div className="w-screen flex justify-items-center items-center flex-col mt-5">
-          <div className="md: mt-5  w-full accent-gradient flex flex-col justify-items-center items-center mb-5">
-            <h3 className="text-xl py-5">
-              <b>Charity</b> powered by{" "}
-              <span className="accent-text font-bold">
-                Ether
-                <SiEthereum className="inline text-2xl" />
-              </span>
-            </h3>
-          </div>
+        {isMetaMaskActive ? (
+          <div className="w-screen flex justify-items-center items-center flex-col mt-5">
+            <div className="md: mt-5  w-full accent-gradient flex flex-col justify-items-center items-center mb-5">
+              <h3 className="text-xl py-5">
+                <b>Charity</b> powered by{" "}
+                <span className="accent-text font-bold">
+                  Ether
+                  <SiEthereum className="inline text-2xl" />
+                </span>
+              </h3>
+            </div>
 
-          <SearchBar queryItems={queryItems} setQueryItems={setQueryItems} />
-          <CreateWalkathonFactory />
-        </div>
+            <SearchBar queryItems={queryItems} setQueryItems={setQueryItems} />
+            <CreateWalkathonFactory />
+          </div>
+        ) : (
+          <NoMetaMask />
+        )}
       </Page>
     </DarkModeProvider>
   );
