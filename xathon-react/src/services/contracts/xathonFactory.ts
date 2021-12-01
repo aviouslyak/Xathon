5bd5591219a6d29a51922590a96245f7513db5aa
Avichai Levenson <avichailevenson@gmail.com>
2021-11-30 21:31:44 -0900

    Update to new rinkeby address, new ABI, and new requestAccounts vs getAccouts

--- xathon-react/src/services/contracts/xathonFactory.ts
+++ xathon-react/src/services/contracts/xathonFactory.ts
@@ -4,70 +4,10 @@ import { XathonFactory } from "../../types/web3-v1-contracts/XathonFactory";
 
 const addresses = {
   localhost: "0x80dbA3D7D54ea6dA24737eCa742d7a2F29Df8AE8",
-  rinkeby: "0xE64DC9d21522f54783Ede9cA54D2Ab311b37F3De",
+  rinkeby: "0x7d6a89f57cF666e613baA13a1cce354237b070b0",
 };
 
 const abi: AbiItem[] = [
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "minimum",
-        type: "uint256",
-      },
-      {
-        internalType: "string",
-        name: "name",
-        type: "string",
-      },
-      {
-        internalType: "string",
-        name: "description",
-        type: "string",
-      },
-    ],
-    name: "deployWalkathon",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "string",
-        name: "name",
-        type: "string",
-      },
-    ],
-    name: "getAddress",
-    outputs: [
-      {
-        internalType: "contract Walkathon",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getDeployedWalkathons",
-    outputs: [
-      {
-        internalType: "string[]",
-        name: "",
-        type: "string[]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
   {
     inputs: [
       {
@@ -82,6 +22,71 @@ const abi: AbiItem[] = [
         internalType: "bool",
         name: "",
         type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "minimum",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "unit",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+    ],
+    name: "deployXathon",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getDeployedXathons",
+    outputs: [
+      {
+        internalType: "string[]",
+        name: "",
+        type: "string[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+    ],
+    name: "getAddress",
+    outputs: [
+      {
+        internalType: "contract Xathon",
+        name: "",
+        type: "address",
       },
     ],
     stateMutability: "view",
@@ -107,7 +112,7 @@ class WalkathonFactory {
   }
 
   static async deployXathon(values: WalkathonValues) {
-    const addresses = await web3.eth.getAccounts();
+    const addresses = await web3.eth.requestAccounts();
     return await this.wFactory.methods
       .deployXathon(
         values.address,
