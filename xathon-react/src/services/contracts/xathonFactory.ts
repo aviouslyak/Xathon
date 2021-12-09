521ecd057e57ebc0989cc88076d5318f55fdc42b
Avichai Levenson <avichailevenson@gmail.com>
2021-12-09 12:57:53 -0900

    require json instead of copy and paste

--- xathon-react/src/services/contracts/xathonFactory.ts
+++ xathon-react/src/services/contracts/xathonFactory.ts
@@ -1,98 +1,13 @@
 import web3 from "../web3";
-import { AbiItem } from "web3-utils";
 import { XathonFactory as XathonFactoryType } from "../../types/web3-v1-contracts/XathonFactory";
+const contractJson = require("../../build/contracts/XathonFactory.json");
 
 const addresses = {
   localhost: "0x80dbA3D7D54ea6dA24737eCa742d7a2F29Df8AE8",
   rinkeby: "0x7d6a89f57cF666e613baA13a1cce354237b070b0",
 };
 
-const abi: AbiItem[] = [
-  {
-    inputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    name: "takenNames",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "minimum",
-        type: "uint256",
-      },
-      {
-        internalType: "string",
-        name: "unit",
-        type: "string",
-      },
-      {
-        internalType: "string",
-        name: "name",
-        type: "string",
-      },
-      {
-        internalType: "string",
-        name: "description",
-        type: "string",
-      },
-    ],
-    name: "deployXathon",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "getDeployedXathons",
-    outputs: [
-      {
-        internalType: "string[]",
-        name: "",
-        type: "string[]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "string",
-        name: "name",
-        type: "string",
-      },
-    ],
-    name: "getAddress",
-    outputs: [
-      {
-        internalType: "contract Xathon",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-];
+const abi = contractJson.abi;
 
 interface XathonValues {
   address: string;
