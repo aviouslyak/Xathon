c6cdcae96e19ecbe933f010e3c446a7c7c95c4bd
Avichai Levenson <avichailevenson@gmail.com>
2021-12-13 11:55:27 -0900

    Installed typescript, tailwind, removed uneeded files

---
+++ xathon-react2/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="bg-slate-500">
+      <header className="">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
