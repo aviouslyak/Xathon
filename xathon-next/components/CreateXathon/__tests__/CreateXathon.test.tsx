ef7104f298054d52ffe1f419941c687dc3d87374
Avichai Levenson <avichailevenson@gmail.com>
2021-12-14 17:31:41 -0900

    Finished test update for createXathon

--- xathon-next/components/CreateXathon/__tests__/CreateXathon.test.tsx
+++ xathon-next/components/CreateXathon/__tests__/CreateXathon.test.tsx
@@ -2,9 +2,11 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import CreateXathon from "../CreateXathon";
 import XathonFactory from "../../../services/contracts/xathonFactory";
 import { TransactionReceipt } from "web3-core";
+import Router from "next/router";
 
 jest.mock("../../../services/contracts/xathonFactory");
 const ADDRESS = "0x0000000000000000000000000000000000000000";
+
 describe("foo", () => {
   let form: HTMLElement;
   let submit: HTMLElement;
@@ -14,10 +16,35 @@ describe("foo", () => {
   let unitInput: HTMLElement;
   let minimumDepositInput: HTMLElement;
   let mockSetAddress: jest.Mock;
+  let spiedDeployXathon: jest.SpyInstance<Promise<TransactionReceipt>>;
+  let spiedRouterPush: jest.SpyInstance;
 
+  const fillAndSubmit = ({
+    address = ADDRESS,
+    name = "Foo",
+    description = "Foo",
+    unit = "miles",
+    minimumDeposit = 0,
+  }: {
+    address?: string | null;
+    name?: string | null;
+    description?: string | null;
+    unit?: string | null;
+    minimumDeposit?: number | null;
+  }) => {
+    address && fireEvent.change(addressInput, { target: { value: address } });
+    name && fireEvent.change(nameInput, { target: { value: name } });
+    description &&
+      fireEvent.change(descriptionInput, { target: { value: description } });
+    unit && fireEvent.change(unitInput, { target: { value: unit } });
+    minimumDeposit &&
+      fireEvent.change(minimumDepositInput, {
+        target: { value: minimumDeposit },
+      });
+    fireEvent.click(submit);
+  };
   beforeEach(() => {
-    mockSetAddress = jest.fn().mockReturnValue(ADDRESS);
-    render(<CreateXathon setAddress={mockSetAddress} />);
+    render(<CreateXathon />);
     form = screen.getByTestId("form");
     addressInput = screen.getByLabelText("Donation recipient address");
     nameInput = screen.getByLabelText("Xathon name");
@@ -25,32 +52,25 @@ describe("foo", () => {
     unitInput = screen.getByLabelText("Unit for xathon");
     minimumDepositInput = screen.getByLabelText("Minimum deposit");
     submit = screen.getByRole("button");
+
+    spiedDeployXathon = jest.spyOn(XathonFactory.prototype, "deployXathon");
+    spiedRouterPush = jest.spyOn(Router, "push");
   });
 
   test("matches snapshot", () => {
     expect(form).toMatchSnapshot();
   });
 
-  test("address input is required", async () => {
-    fireEvent.change(nameInput, { target: { value: "Foo " } });
-    fireEvent.change(descriptionInput, { target: { value: "Foo " } });
-    fireEvent.change(unitInput, { target: { value: "miles " } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-
-    fireEvent.click(submit);
+  test("address field is required", async () => {
+    fillAndSubmit({ address: null });
     await waitFor(() => {
       const errorMessage = screen.getByText("*Address is required");
       expect(errorMessage).toBeInTheDocument();
     });
   });
 
-  test("address input must be of type address", async () => {
-    fireEvent.change(addressInput, { target: { value: "not valid" } });
-    fireEvent.change(nameInput, { target: { value: "Foo " } });
-    fireEvent.change(descriptionInput, { target: { value: "Foo " } });
-    fireEvent.change(unitInput, { target: { value: "miles " } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+  test("address field must be of type ethereum address", async () => {
+    fillAndSubmit({ address: "not valid" });
 
     await waitFor(() => {
       const errorMessage = screen.getByText("*Must by an ethereum address");
@@ -58,12 +78,8 @@ describe("foo", () => {
     });
   });
 
-  test("name input is required", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(descriptionInput, { target: { value: "Foo " } });
-    fireEvent.change(unitInput, { target: { value: "miles " } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+  test("name field is required", async () => {
+    fillAndSubmit({ name: null });
 
     await waitFor(() => {
       const errorMessage = screen.getByText("*Name is required");
@@ -71,43 +87,28 @@ describe("foo", () => {
     });
   });
 
-  test("name input can be less than 50 characters", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "a".repeat(30) } });
-    fireEvent.change(descriptionInput, { target: { value: "Foo " } });
-    fireEvent.change(unitInput, { target: { value: "miles " } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+  test("name field can be less than 50 characters", async () => {
+    fillAndSubmit({ name: "a".repeat(30) });
 
     await waitFor(() => {
       const errorMessage = screen.queryByText("*Name is required");
       expect(errorMessage).not.toBeInTheDocument();
-      expect(XathonFactory.deployXathon).toHaveBeenCalledTimes(1);
+      expect(spiedDeployXathon).toHaveBeenCalled();
     });
   });
 
-  test("name input can be 50 characters", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "a".repeat(50) } });
-    fireEvent.change(descriptionInput, { target: { value: "Foo " } });
-    fireEvent.change(unitInput, { target: { value: "miles " } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+  test("name field can be 50 characters", async () => {
+    fillAndSubmit({ name: "a".repeat(50) });
 
     await waitFor(() => {
       const errorMessage = screen.queryByText("*Name is required");
       expect(errorMessage).not.toBeInTheDocument();
-      expect(XathonFactory.deployXathon).toHaveBeenCalledTimes(1);
+      expect(spiedDeployXathon).toHaveBeenCalled();
     });
   });
 
-  test("name input can not be more than 50 characters", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "a".repeat(51) } });
-    fireEvent.change(descriptionInput, { target: { value: "Foo " } });
-    fireEvent.change(unitInput, { target: { value: "miles " } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+  test("name field can not be more than 50 characters", async () => {
+    fillAndSubmit({ name: "a".repeat(51) });
 
     await waitFor(() => {
       const errorMessage = screen.getByText(
@@ -117,59 +118,39 @@ describe("foo", () => {
     });
   });
 
-  test("description textarea is not required", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "foo" } });
-    fireEvent.change(unitInput, { target: { value: "miles " } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+  test("description field is not required", async () => {
+    fillAndSubmit({ description: null });
 
     await waitFor(() => {
-      expect(XathonFactory.deployXathon).toHaveBeenCalledTimes(1);
+      expect(XathonFactory.prototype.deployXathon).toHaveBeenCalledTimes(1);
     });
   });
 
-  test("unit input can be less than 20 characters", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "a" } });
-    fireEvent.change(descriptionInput, { target: { value: "Foo " } });
-    fireEvent.change(unitInput, { target: { value: "u".repeat(10) } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+  test("unit field can be less than 20 characters", async () => {
+    fillAndSubmit({ unit: "a".repeat(10) });
 
     await waitFor(() => {
       const errorMessage = screen.queryByText(
         "*Must be less than 20 characters in length"
       );
       expect(errorMessage).not.toBeInTheDocument();
-      expect(XathonFactory.deployXathon).toHaveBeenCalledTimes(1);
+      expect(XathonFactory.prototype.deployXathon).toHaveBeenCalledTimes(1);
     });
   });
 
-  test("unit input can be 20 characters", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "a" } });
-    fireEvent.change(descriptionInput, { target: { value: "Foo " } });
-    fireEvent.change(unitInput, { target: { value: "u".repeat(20) } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
-
+  test("unit field can be 20 characters", async () => {
+    fillAndSubmit({ unit: "a".repeat(20) });
     await waitFor(() => {
       const errorMessage = screen.queryByText(
         "*Must be less than 20 characters in length"
       );
       expect(errorMessage).not.toBeInTheDocument();
-      expect(XathonFactory.deployXathon).toHaveBeenCalledTimes(1);
+      expect(XathonFactory.prototype.deployXathon).toHaveBeenCalledTimes(1);
     });
   });
 
-  test("unit input can not be more than 20 characters", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "a" } });
-    fireEvent.change(descriptionInput, { target: { value: "Foo " } });
-    fireEvent.change(unitInput, { target: { value: "u".repeat(21) } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+  test("unit field can not be more than 20 characters", async () => {
+    fillAndSubmit({ unit: "a".repeat(21) });
 
     await waitFor(() => {
       const errorMessage = screen.getByText(
@@ -179,33 +160,28 @@ describe("foo", () => {
     });
   });
 
-  test("minimumDeposit input is not required", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "foo" } });
-    fireEvent.change(descriptionInput, { target: { value: "foo" } });
-    fireEvent.change(unitInput, { target: { value: "miles " } });
-    fireEvent.click(submit);
+  test("minimumDeposit field is not required", async () => {
+    fillAndSubmit({ minimumDeposit: null });
 
     await waitFor(() => {
-      expect(XathonFactory.deployXathon).toHaveBeenCalledTimes(1);
+      expect(XathonFactory.prototype.deployXathon).toHaveBeenCalledTimes(1);
     });
   });
 
   test("successfully calls deployXathon with correct values", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "foo" } });
-    fireEvent.change(descriptionInput, { target: { value: "foo" } });
-    fireEvent.change(unitInput, { target: { value: "miles" } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
-
+    fillAndSubmit({
+      name: "N",
+      description: "D",
+      unit: "M",
+      minimumDeposit: 0,
+    });
     await waitFor(() => {
-      expect(XathonFactory.deployXathon).toHaveBeenCalledTimes(1);
-      expect(XathonFactory.deployXathon).toHaveBeenCalledWith({
+      expect(XathonFactory.prototype.deployXathon).toHaveBeenCalledTimes(1);
+      expect(XathonFactory.prototype.deployXathon).toHaveBeenCalledWith({
         address: ADDRESS,
-        name: "foo",
-        description: "foo",
-        unit: "miles",
+        name: "N",
+        description: "D",
+        unit: "M",
         minimumDeposit: 0,
       });
     });
@@ -221,53 +197,27 @@ describe("foo", () => {
     expect(defaults.length).toBe(4);
   });
 
-  test("Changes button text after submitting", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "foo" } });
-    fireEvent.change(descriptionInput, { target: { value: "foo" } });
-    fireEvent.change(unitInput, { target: { value: "miles" } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+  test("redirects after submitting", async () => {
+    fillAndSubmit({});
 
     await waitFor(() => {
-      const buttonRefound = screen.getByText(/create/i);
-      expect(buttonRefound).toBeInTheDocument();
+      expect(spiedRouterPush).toHaveBeenCalled();
+      expect(spiedRouterPush).toHaveBeenCalledWith(`/${ADDRESS}`);
     });
   });
 
-  test("Sets loading text when loading", async () => {
-    const mockFn = jest.spyOn(XathonFactory, "deployXathon");
-    mockFn.mockImplementation(async () => {
-      return await new Promise(() => {});
-    });
-
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "foo" } });
-    fireEvent.change(descriptionInput, { target: { value: "foo" } });
-    fireEvent.change(unitInput, { target: { value: "miles" } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
-
+  test("sets loading text when loading", async () => {
+    fillAndSubmit({});
     await waitFor(() => {
       const buttonRefound = screen.getByText(/submitting/i);
       expect(buttonRefound).toBeInTheDocument();
     });
   });
 
-  test("Error is shown", async () => {
-    const mockFn = jest.spyOn(XathonFactory, "deployXathon");
-    mockFn.mockImplementation(async () => {
-      return await new Promise((resolve, reject) => {
-        reject();
-      });
-    });
+  test("error is shown when deployXathon fails", async () => {
+    spiedDeployXathon.mockRejectedValueOnce("foo");
 
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "foo" } });
-    fireEvent.change(descriptionInput, { target: { value: "foo" } });
-    fireEvent.change(unitInput, { target: { value: "miles" } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
+    fillAndSubmit({});
 
     await waitFor(() => {
       const errorText = screen.getByText(
@@ -277,24 +227,10 @@ describe("foo", () => {
     });
   });
 
-  test("Error is not shown by default", () => {
+  test("error is not shown by default", () => {
     const errorText = screen.queryByText(
       "Some error has occurred. Check that you confirmed the transaction in MetaMask, and that the contract name is unique"
     );
     expect(errorText).not.toBeInTheDocument();
-  });
-
-  test("calls setAddress with address on success", async () => {
-    fireEvent.change(addressInput, { target: { value: ADDRESS } });
-    fireEvent.change(nameInput, { target: { value: "foo" } });
-    fireEvent.change(descriptionInput, { target: { value: "foo" } });
-    fireEvent.change(unitInput, { target: { value: "miles" } });
-    fireEvent.change(minimumDepositInput, { target: { value: 0 } });
-    fireEvent.click(submit);
-
-    await waitFor(() => {
-      expect(mockSetAddress).toHaveBeenCalled();
-      expect(mockSetAddress).toHaveBeenCalledWith(ADDRESS);
-    });
   });
 });
