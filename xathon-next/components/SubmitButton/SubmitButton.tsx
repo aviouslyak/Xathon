6f93148870cc6fb37aec61610c1894e2c6aab981
Avichai Levenson <avichailevenson@gmail.com>
2022-12-08 10:17:59 -0700

    button class

--- xathon-next/components/SubmitButton/SubmitButton.tsx
+++ xathon-next/components/SubmitButton/SubmitButton.tsx
@@ -6,7 +6,7 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 const SubmitButton: React.FC<Props> = ({ className, ...props }) => {
   return (
     <button
-      className={`${className} group w-40 h-10 text-xl font-semibold flex items-center justify-center rounded-lg transition-shadow shadow-md hover:shadow-lg shadow-pink-900 hover:shadow-pink-900 bg-orange-400`}
+      className={`${className} button w-40 h-10 text-xl`}
       type="submit"
       {...props}
     >
